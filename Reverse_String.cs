39254731ec1320469c8214deb464084573eb10a7
Jeffrey Padgett <jeffrey_padgett@Jeffreys-MacBook-Pro.local>
2018-12-26 08:01:31 -0500

    Practiced 3 simple coding challenges

---
+++ Reverse_String.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Math;
+
+namespace Keep_Hydrated
+{
+    class MainClass
+
+/*Because Nathan knows it is important to stay hydrated, he drinks 0.5 litres of water per hour of cycling.
+You get given the time in hours and you need to return the number of litres Nathan will drink, 
+rounded to the smallest value.
+
+For example:
+
+time = 3 ----> litres = 1
+
+time = 6.7---> litres = 3
+
+time = 11.8--> litres = 5 */
+
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine(Litres(5.8);
+
+        }
+
+        public static int Litres(double time)
+        {
+
+            return double litres = math.floor(0.5 * time);
+        }
+
+
+    }
+}
---
+++ SumOfPositives.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace Code_Wars
+{
+
+   //You get an array of numbers, return the sum of all of the positives ones.
+
+//   Example [1,-4,7,12] => 1 + 7 + 12 = 20
+
+//   Note: if there is nothing to sum, the sum is default to 0.
+
+
+    class MainClass
+    {
+        public int sum;
+        
+        public static void Main(string[] args)
+        {
+
+        }
+
+        public static int PositiveSum(int[] arr)
+        {
+            //find a place to store the int
+            int sum = 0;
+            foreach (int number in arr)
+            {
+                if (number >= 0)
+                    sum += number;
+            }
+
+            return sum;
+        }
+
+        //Best Practice is to use a LINQ return arr.Where(x => x > 0).Sum();
+    }
+}
