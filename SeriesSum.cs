0b80009ec96f8d0dede07cc27da8a99f99a37748
Jeffrey Padgett <jeffrey_padgett@Jeffreys-MacBook-Pro.local>
2018-12-26 18:06:14 -0500

    Kata level 7

---
+++ SeriesSum.cs
@@ -0,0 +1,49 @@
+﻿using System;
+namespace Reverse_String
+
+/*Task:
+Your task is to write a function which returns the sum of following series upto nth term(parameter).
+
+Series: 1 + 1/4 + 1/7 + 1/10 + 1/13 + 1/16 +...
+Rules:
+You need to round the answer to 2 decimal places and return it as String.
+
+If the given value is 0 then it should return 0.00
+
+You will only be given Natural Numbers as arguments.*/
+
+
+{
+
+    public class NthSedries
+    {
+
+
+
+
+        public static class NthSeries
+        {
+
+            public static string seriesSum(int n)
+            {
+                double result = 0;
+                double reverage = 1;
+                for (int i = 0; i < n; i += 1)
+                {
+                    if (i == 0)
+                    {
+                        result = 1;
+                    }
+                    else
+                    {
+                        reverage += 3;
+                        result = result + (1 / reverage);
+                    }
+                }
+                result = Math.Round(result, 2);
+                return result.ToString("N2");
+            }
+        }
+
+    }
+}
