2c49c926bc5d4fa95b0eea81cb52c8e0d900e6d2
JeffPadgett <JeffreyPadgett1@gmail.com>
2018-12-27 06:09:52 -0500

    CountingSheep

---
+++ CountingSheep.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+
+
+/*Consider an array of sheep where some sheep may be missing from their place. 
+ * We need a function that counts the number of sheep present in the array 
+ * (true means present).*/
+
+
+namespace Reverse_String
+{
+
+
+    public class CountingSheep
+    {
+
+        private static int SheepCounter(bool[] sheepArray)
+
+        {
+
+            int numberOfSheep = 0;
+            foreach (var sheep in sheepArray)
+            {
+                if (sheep)
+                    numberOfSheep++;
+            }
+
+            return numberOfSheep;
+
+        }
+
+        //Best Practice     return sheepArray.Count(s => s);
+
+
+
+    }
+
+
+}
