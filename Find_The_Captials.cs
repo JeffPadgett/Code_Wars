cc4e334f973001d850776d21329fa9c1482e1f64
JeffPadgett <jp17129@aol.com>
2018-12-29 20:28:32 -0500

    Find_The_Captials First code wars example where my function adheared to best practices

---
+++ Find_The_Captials.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace ConsoleApp7
+{
+    class Program
+    {
+        //Instructions
+
+        //Write a function that takes a single string (word) as argument.The function must return 
+        //an ordered list containing the indexes of all capital letters in the string.
+
+        public static int[] Capitals(string word)
+        {
+
+            List<int> CapList = new List<int>();
+            for (int i = 0; i < word.Length; i++)
+            {
+                if (char.IsUpper(word[i]))
+                    CapList.Add(i);
+            }
+
+            return CapList.ToArray();
+        }
+
+    }
+}
