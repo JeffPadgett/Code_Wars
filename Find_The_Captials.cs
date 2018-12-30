2e7eb86930612faae34ceccf254009f07a1b0b09
JeffPadgett <JeffreyPadgett1@gmail.com>
2018-12-29 20:35:27 -0500

    Update

--- Find_The_Captials.cs
+++ Find_The_Captials.cs
@@ -9,7 +9,7 @@ namespace ConsoleApp7
 {
     class Program
     {
-        //Instructions
+        //Instructions:
 
         //Write a function that takes a single string (word) as argument.The function must return 
         //an ordered list containing the indexes of all capital letters in the string.
