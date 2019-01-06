8f6a354bedc32ee9f06edb792330ab4861365f5d
JeffPadgett <JeffreyPadgett1@gmail.com>
2019-01-06 17:02:08 -0500

    itterating through an array to instantiate a dictionary

---
+++ Find_The_Odd_Int.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp8
+{
+
+    //Directions:
+
+    //Given an array, find the int that appears an odd number of times.
+    //There will always be only one integer that appears an odd number of times.
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+
+        public static int find_it(int[] seq)
+        {
+            //create a list or a dictionary that stores an itteration of a number each time that it iterated through. 
+            Dictionary<string, int>  odd_Finder = new Dictionary<string, int>();
+            //use a for each loop to itterate through the parameter array, for each number, which is a key, assign a new key,
+            //with the default value of one, each additional iteration increment the value of the key by one. 
+            //If the value currently exist, then that value +=1. 
+            foreach (int )
+
+            //Use a method to discover which key has a value that is odd. 
+
+            //return that value
+        }
+
+
+
+
+        /* --------------------------------------------------Best Practices---------------------------------------------------------
+
+
+
+        ----------------------------------------------------------------------------------------------------------------------------*/
+    }
+}
