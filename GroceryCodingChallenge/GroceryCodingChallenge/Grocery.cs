b6ac90bd6d1210c9e69927ed0317d2d90a955266
JeffPadgett <JeffreyPadgett1@gmail.com>
2019-01-09 23:08:26 -0500

    Intro Commit

---
+++ GroceryCodingChallenge/GroceryCodingChallenge/Grocery.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GroceryCodingChallenge
+{
+    public class Grocery
+    {
+        protected string name;
+        protected float weight;
+
+        protected enum GroceryType
+        {
+            Produce = 0,
+            Meat = 1,
+            Dairy = 2,
+        }
+
+    }
+}
