9d622c1f777d870a9216c1a57fc2209ec151ee27
JeffPadgett <JeffreyPadgett1@gmail.com>
2019-02-06 01:35:45 -0500

    EnoughIsEnough

---
+++ EnoughIsEnough.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EnoughIsEnough
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+
+        public class Kata
+        {
+            public static int[] DeleteNth(int[] arr, int x)
+            {
+                if (x <= 0)
+                    return new int[0];
+                List<int> listArr = new List<int>();
+                listArr = arr.ToList();
+                var newListArr = new List<int>();
+
+                foreach (int num in listArr)
+                {
+                    int numCount = newListArr.Where(y => y == num).Count(); //var that determines the amount of times an elemment appears in an array.
+                    if (numCount < x)
+                        newListArr.Add(num);
+                }
+                return newListArr.ToArray();
+            }
+        }
+
+    }
+}
